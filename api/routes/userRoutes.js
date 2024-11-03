248865e5d3618cbaf32e2d0b6d6a9558e74bc72e
martinlgalva <108037810+martinlgalvan@users.noreply.github.com>
2024-11-03 12:22:52 -0300

    rollback

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,14 +1,24 @@
 // Elimina multer y la configuración del archivo
 import express from 'express';
-import { uploadBlogController, getBlogByIdController, getAllBlogsController, updateBlogController, deleteBlogController } from './../controllers/blogController.js';
-import { register, login, logout,getAllUsersController, updateUserAccessController, deleteUserController  } from '../controllers/userController.js';
+import { 
+    register, 
+    login, 
+    logout,
+    getAllUsersController, 
+    updateUserAccessController, 
+    deleteUserController 
+} from '../controllers/userController.js';
+
+
 import { getOEmbed } from '../controllers/vimeoController.js';
-import upload from '../multerConfig.js';
-import multer from 'multer';
 
-
-// Configuración de almacenamiento para Multer
-
+import { 
+    uploadBlogController, 
+    getBlogByIdController, 
+    getAllBlogsController, 
+    updateBlogController, 
+    deleteBlogController 
+} from './../controllers/blogController.js';
 
 const router = express.Router();
 
@@ -19,10 +29,10 @@ router.post('/api/logout', logout)
 router.get('/api/users', getAllUsersController);
 
 // Elimina multer de las rutas de subida
-router.post('/api/blogs/upload', upload.single('file'), uploadBlogController);
+router.post('/api/blogs/upload', uploadBlogController); // Sin multer
 router.get('/api/blogs/:id', getBlogByIdController);
 router.get('/api/blogs', getAllBlogsController);
-router.put('/api/blogs/:id', upload.single('file'), updateBlogController);
+router.put('/api/blogs/:id', updateBlogController); // Sin multer
 router.delete('/api/blogs/:id', deleteBlogController);
 router.delete('/api/users/:userId', deleteUserController);
 router.post('/api/oembed', getOEmbed);
